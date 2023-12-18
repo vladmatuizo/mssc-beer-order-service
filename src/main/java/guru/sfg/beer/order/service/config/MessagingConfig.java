8e43c29e34e1550d56240438ce68a29fd94f71b2
Vladislav Matuizo <dalvmatujzo@gmail.com>
2023-12-18 01:01:54 +0100

    Fix jackson converter config for messages, change message broker dependency to artemis

--- src/main/java/guru/sfg/beer/order/service/config/MessagingConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/MessagingConfig.java
@@ -1,5 +1,6 @@
 package guru.sfg.beer.order.service.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -10,8 +11,10 @@ import org.springframework.jms.support.converter.MessageType;
 public class MessagingConfig {
 
     @Bean
-    public MessageConverter jacksonJmsMessageConverter() {
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+        converter.setObjectMapper(objectMapper);
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
